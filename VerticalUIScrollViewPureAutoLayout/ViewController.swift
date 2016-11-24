5cd1dbbea845a1fccc26858f30aa662640cbe08f
Fernando Fernandes <fernando.fernandes@hp.com>
2016-11-24 15:14:56 -0200

    Updated to Xcode 8.1 and Swift 3.

--- VerticalUIScrollViewPureAutoLayout/ViewController.swift
+++ VerticalUIScrollViewPureAutoLayout/ViewController.swift
@@ -3,23 +3,11 @@
 //  VerticalUIScrollViewPureAutoLayout
 //
 //  Created by Fernando Fernandes on 12/27/14.
-//  Copyright (c) 2014 Damaa. All rights reserved.
+//  Copyright (c) 2014 singledev. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
